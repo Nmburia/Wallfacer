c60300ba239aaebd082ac530a73ceb6c294d112e
Nathan Mburia <nathanmburia@gmail.com>
2024-10-22 19:44:00 +0100

    2024-10-22 added collision code but not working yet

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use winit::window::Window;
 use winit::{
     dpi::LogicalSize,
-    event::{Event, WindowEvent, VirtualKeyCode},
+    event::{Event, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
 use colored::Colorize;
 
-use rand::Rng;
 use rand;
+use rand::Rng;
 use std::time::{Duration, Instant};
 
 use glam::f32::Vec2;
@@ -34,15 +34,14 @@ const BLUR_RADIUS: usize = 5;
 // use std::time::{Duration, Instant};
 //
 // fn main() {
-    // let start = Instant::now();
-    // expensive_function();
-    // let duration = start.elapsed();
+// let start = Instant::now();
+// expensive_function();
+// let duration = start.elapsed();
 
-    // println!("Time elapsed in expensive_function() is: {:?}", duration);
+// println!("Time elapsed in expensive_function() is: {:?}", duration);
 // }
 
 fn main() {
-
     let event_loop = EventLoop::new();
     let window = {
         let size = LogicalSize::new(WIDTH as u32, HEIGHT as u32);
@@ -53,57 +52,83 @@ fn main() {
             .unwrap()
     };
 
-
-
-
-
     // let mut planet = Planet {pos: Vec2::new(200.0, 200.0), radius: 5.0, vel: Vec2::new(0.0, 0.0), mass: 100000.1};
-    let mut planet = Planet::new("Earth", Vec2::new(200.0, 200.0), 5.0, Vec2::new(0.0, 0.0), 100_000.1, PlanetColor::red());
-
+    let mut planet = Planet::new(
+        "Earth",
+        Vec2::new(200.0, 200.0),
+        5.0,
+        Vec2::new(0.0, 0.0),
+        100_000.1,
+        PlanetColor::red(),
+    );
 
     // good values between two planets are 1.1 and  10000000000000.0
     // let mut planet2 = Planet {pos: Vec2::new(400.0, 300.0), radius: 10.0, vel: Vec2::new(0.05, 0.0), mass: 10_000_000_000_000_0.0};
-    let mut planet2 = Planet::new("Sun", Vec2::new(400.0, 300.0), 10.0, Vec2::new(0.0, 0.0), 10_000_000_000.0, PlanetColor::white());
+    let mut planet2 = Planet::new(
+        "Sun",
+        Vec2::new(400.0, 300.0),
+        10.0,
+        Vec2::new(0.0, 0.0),
+        10_000_000_000.0,
+        PlanetColor::white(),
+    );
     let init_vel = calc_init_orbital_velocity(&planet, &planet2);
     planet.vel = init_vel;
 
-    println!("Will escape orbit: {}", check_escape_velocity(&planet, &planet2));
+    println!(
+        "Will escape orbit: {}",
+        check_escape_velocity(&planet, &planet2)
+    );
 
-    let mut planet3 = Planet::new("Moon", Vec2::new(500.0, 500.0), 3.0, Vec2::new(0.0, 0.0), 1000.5, PlanetColor::blue());
-    let init_vel = calc_init_orbital_velocity(&planet3, &planet2);
-    planet3.vel = init_vel;
+    let mut planet3 = Planet::new(
+        "Moon",
+        Vec2::new(500.0, 500.0),
+        3.0,
+        Vec2::new(0.0, 0.0),
+        1000.5,
+        PlanetColor::blue(),
+    );
+    // let init_vel = calc_init_orbital_velocity(&planet3, &planet2);
+    // planet3.vel = init_vel;
 
-    println!("Will escape orbit: {}", check_escape_velocity(&planet3, &planet2));
+    println!(
+        "Will escape orbit: {}",
+        check_escape_velocity(&planet3, &planet2)
+    );
 
-    let  planet4 = Planet::create_satellite(&planet2, "Satellite", 8.0, 400.0, PlanetColor::green());
-    let planet5 = Planet::create_satellite(&planet, "Tiny Satellite", 2.0, 100.0, PlanetColor::new(150, 14, 21, 255));
-    
+    // let planet4 = Planet::create_satellite(&planet2, "Satellite", 8.0, 400.0, PlanetColor::green());
+    // let planet5 = Planet::create_satellite(
+    // &planet,
+    // "Tiny Satellite",
+    // 2.0,
+    // 100.0,
+    // PlanetColor::new(150, 14, 21, 255),
+    // );
 
     let mut pixels = create_pixel_buffer(&window, WIDTH as u32, HEIGHT as u32);
 
     pixels.clear_color(Color::BLACK);
 
-    let mut planet_list = Box::new(vec![planet, planet2, planet3, planet4, planet5]);
-    // let mut planet_list: &mut Vec<Planet> = &mut planets; 
-    
+    let mut planet_list = Box::new(vec![planet, planet2, planet3]);
+    // let mut planet_list: &mut Vec<Planet> = &mut planets;
 
     event_loop.run(move |event, _, control_flow| {
-
-    
-    // let mut planet_list: Rc<RefCell<Vec<&mut Planet>>> = Rc::new(RefCell::new(vec![&mut planet,  &mut planet2]));
+        // let mut planet_list: Rc<RefCell<Vec<&mut Planet>>> = Rc::new(RefCell::new(vec![&mut planet,  &mut planet2]));
 
         match event {
             Event::MainEventsCleared => {
                 pixels.frame_mut().fill(0 as u8);
-                let mut accel_list: Vec<Vec2>  = vec![];
+                let mut accel_list: Vec<Vec2> = vec![];
                 for s in planet_list.iter() {
                     let mut accel = Vec2::new(0.0, 0.0);
-                    for  p in planet_list.iter() {
+                    for p in planet_list.iter() {
                         if s == p {
                             continue;
                         }
+                        if check_collision(&s, &p) {
+                            accel += calc_collision(s, p);
+                        }
                         accel += calc_accel(&s, &p);
-                        
                     }
                     // println!("{:?}", accel);
                     accel_list.push(accel);
@@ -112,23 +137,22 @@ fn main() {
                     planet_list[i].update(accel_list[i]);
                     planet_list[i].render(&mut pixels);
                 }
-            // let bright_areas = extract_bright_areas(pixels.frame());
+                // let bright_areas = extract_bright_areas(pixels.frame());
 
-    // Step 2: Apply Gaussian blur to the bright areas
-            // let blurred_bright_areas = gaussian_blur(&bright_areas);
-            //
-    // Step 3: Combine the original frame with the blurred bright areas
-            // combine_images(&mut pixels, &blurred_bright_areas);
-            pixels.render();
-        }
-        Event::WindowEvent {
-                event: WindowEvent::CloseRequested, .. } => {
+                // Step 2: Apply Gaussian blur to the bright areas
+                // let blurred_bright_areas = gaussian_blur(&bright_areas);
+                //
+                // Step 3: Combine the original frame with the blurred bright areas
+                // combine_images(&mut pixels, &blurred_bright_areas);
+                pixels.render();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
                 *control_flow = ControlFlow::Exit;
             }
-        _ => {}
+            _ => {}
         }
     });
-    
 }
-
-
--- src/physics.rs
+++ src/physics.rs
@@ -1,18 +1,20 @@
-use glam::Vec2;
 use crate::planet::Planet;
+use glam::Vec2;
 
 pub fn calc_accel(self_planet: &Planet, planet: &Planet) -> Vec2 {
     let G: f32 = 6.6 * (10.0_f32).powf(-11.0_f32);
     let mut resultant_force = Vec2::new(0.0, 0.0);
-        //ma = Gmm/r^2   -> a = Gm/r^2
-    let dist = ((planet.pos.x - self_planet.pos.x).powf(2.0) + (planet.pos.y - self_planet.pos.y).powf(2.0)).sqrt().abs(); 
+    //ma = Gmm/r^2   -> a = Gm/r^2
+    let dist = ((planet.pos.x - self_planet.pos.x).powf(2.0)
+        + (planet.pos.y - self_planet.pos.y).powf(2.0))
+    .sqrt()
+    .abs();
     let magnitude = (G * self_planet.mass * planet.mass) / dist.powi(2);
     let force = (planet.pos - self_planet.pos).normalize_or_zero() * magnitude;
     resultant_force += force;
     let accel = force / self_planet.mass;
     accel
 }
-
 
 pub fn calc_init_orbital_velocity(planet: &Planet, sun: &Planet) -> Vec2 {
     // V = ((Gm)/r).sqrt()
@@ -24,10 +26,12 @@ pub fn calc_init_orbital_velocity(planet: &Planet, sun: &Planet) -> Vec2 {
 }
 
 pub fn check_escape_velocity(planet: &Planet, sun: &Planet) -> bool {
-// checks whether a given planet will escape the orbit of a given sun
+    // checks whether a given planet will escape the orbit of a given sun
     let G: f32 = 6.6 * (10.0_f32).powf(-11.0_f32);
-    let dist = ((sun.pos.x - planet.pos.x).powf(2.0) + (sun.pos.y - planet.pos.y).powf(2.0)).sqrt().abs();
-    let escape_vel = ((2.0 * G * sun.mass) / dist ).sqrt();
+    let dist = ((sun.pos.x - planet.pos.x).powf(2.0) + (sun.pos.y - planet.pos.y).powf(2.0))
+        .sqrt()
+        .abs();
+    let escape_vel = ((2.0 * G * sun.mass) / dist).sqrt();
     if planet.vel.length() > escape_vel {
         true
     } else {
@@ -35,9 +39,23 @@ pub fn check_escape_velocity(planet: &Planet, sun: &Planet) -> bool {
     }
 }
 
+pub fn check_collision(self_planet: &Planet, planet: &Planet) -> bool {
+    (self_planet.pos.distance(planet.pos) < self_planet.radius + planet.radius)
+}
+
+pub fn calc_collision(self_planet: &Planet, planet: &Planet) -> Vec2 {
+    let old_vel = self_planet.vel;
+    let part_a = (2.0 * planet.mass / (self_planet.mass * planet.mass));
+    let part_b = ((self_planet.vel - planet.vel).dot(self_planet.pos - planet.pos))
+        / (self_planet.pos.distance(planet.pos).powi(2));
+    let part_c = self_planet.pos - planet.pos;
+    let collision_force = part_a * part_b * part_c;
+    collision_force
+}
+
 pub fn get_gradient(vel: Vec2) -> f32 {
     let norm = vel.normalize_or_zero();
-    let y_d =  norm.y;
+    let y_d = norm.y;
     let x_d = norm.x;
     y_d / x_d
 }
